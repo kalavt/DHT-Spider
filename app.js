1018448be367f934fe900fd32f48406e01ed312f
kalavt <443311+kalavt@user.noreply.github.com>
2021-04-05 12:59:34 +0800

    refine code

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const Spider = require('dhtspider')
+
+const spider = new Spider({
+    bootstraps: [
+        {address: 'router.bittorrent.com', port: 6881}, 
+        {address: 'dht.transmissionbt.com', port: 6881},
+        {address: 'router.utorrent.com', port: 6881}
+    ],
+    tableCaption: parseInt(process.env.TABLE_CAPTION || 200)
+})
+
+spider.on('ensureHash', (hash, addr)=> console.log(`magnet:?xt=urn:btih:${hash}`))
+
+spider.listen(parseInt(process.env.PORT || 6882))
+
+
