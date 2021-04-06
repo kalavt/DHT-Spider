1492dfe844ea248cb85dfac219ef3dbd3e4187cd
kalavt <443311+kalavt@user.noreply.github.com>
2021-04-06 16:35:20 +0800

    refine code

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const Spider = require("dhtspider");
 const exitHook = require("async-exit-hook");
 require("./spider_extend");
 
+//setup db
+const dynamoose = require("dynamoose");
+dynamoose.aws.ddb.local(process.env.backend);
+var InfoHash = dynamoose.model("InfoHash", {
+  hash: String,
+  downloaded: Number,
+});
+
 const spider = new Spider();
 spider.loadNodes();
 spider.tableCaption = parseInt(process.env.TABLE_CAPTION || 200);
 
-spider.on("ensureHash", (hash, addr) =>
-  console.log(`magnet:?xt=urn:btih:${hash}`)
-);
+spider.on("ensureHash", async (hash, addr) => {
+  await new InfoHash({
+    hash: hash,
+    addr: addr,
+  }).save();
+  console.log(`magnet:?xt=urn:btih:${hash}`);
+});
 
 exitHook((exit) => {
-   spider.saveNodes(exit);
+  spider.saveNodes(exit);
 });
 
 spider.listen(parseInt(process.env.PORT || 6881));
