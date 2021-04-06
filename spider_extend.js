c3f9f04feab601c4d0118e8acd5f89b7f3adecff
kalavt <443311+kalavt@user.noreply.github.com>
2021-04-06 17:15:26 +0800

    refine code

--- spider_extend.js
+++ spider_extend.js
@@ -11,10 +11,34 @@ Spider.prototype.saveNodes = function (cb) {
   fs.writeFile(".session", JSON.stringify(this), cb);
 };
 
+function isBufferLike(x) {
+  return (
+    isObject(x) && x.type === "Buffer" && (isArray(x.data) || isString(x.data))
+  );
+}
+
+function isArray(x) {
+  return Array.isArray(x);
+}
+
+function reviver(key, value) {
+  if (isBufferLike(value)) {
+    if (isArray(value.data)) {
+      return Buffer.from(value.data);
+    } else if (isString(value.data)) {
+      if (value.data.startsWith("base64:")) {
+        return Buffer.from(value.data.slice("base64:".length), "base64");
+      }
+      return Buffer.from(value.data);
+    }
+  }
+  return value;
+}
+
 Spider.prototype.loadNodes = function () {
   fs.readFile(".session", "utf-8", (err, data) => {
     if (!err && data) {
-      var session = JSON.parse(data.toString());
+      var session = JSON.parse(data.toString(),reviver);
       this.token.token = session.token.token;
       this.table.nodes = session.table.nodes;
       this.table.id = session.table.id;
